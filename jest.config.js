ca0c8cbacaa7daeab50968ec000b6cef334ead45
Vedprakash Poddar <vedprakash.kumar@nickelfox.com>
2026-06-11 12:27:16 +0530

    test issue fixed

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+const nextJest = require("next/jest.js");
 
 const createJestConfig = nextJest({ dir: "./" });
 
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
@@ -14,4 +14,4 @@ const config: Config = {
   coverageProvider: "v8",
 };
 
-export default createJestConfig(config);
+module.exports = createJestConfig(config);
